59f60db6b46bc1a54aecd3c4109bfc3023daa166
sanjeevwolfy <sanjeevfirst2015@gmail.com>
2025-02-12 14:36:23 +0530

    Backend initialisation done...

---
+++ nms-backend/nms-backend/src/test/java/sas/nms_backend/NmsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package sas.nms_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NmsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
