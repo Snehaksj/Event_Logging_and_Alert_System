82f348a98bdfae0729fe310f733bfc6d7a4bcba7
Snehaksj <snehaksj@gmail.com>
2025-02-19 14:35:26 +0530

    Removed signup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,6 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './global.css';
 import Dashboard from './Components/Dashboard';
 import LoginPage from './pages/Login';
-import SignupPage from './pages/Signup';
 import { AuthProvider } from './Context/authContext'; // Import AuthProvider
 import ProtectedRoute from './Components/ProtectedRoute'; // Import ProtectedRoute
 
@@ -14,7 +13,6 @@ function App() {
           {/* Protect the Dashboard route */}
           <Route path="/" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
           <Route path="/login" element={<LoginPage />} />
-          <Route path="/signup" element={<SignupPage />} />
         </Routes>
       </Router>
     </AuthProvider>
