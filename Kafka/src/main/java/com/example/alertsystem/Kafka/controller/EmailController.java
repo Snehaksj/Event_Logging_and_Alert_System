d231c42020186343a87afdff6fc8a1ad2dbdeb01
Aakil Shihaf V <aakilshihafv@gmail.com>
2025-02-19 12:11:17 +0530

    essaging service api done

---
+++ Kafka/src/main/java/com/example/alertsystem/Kafka/controller/EmailController.java
@@ -0,0 +1,22 @@
+package com.example.alertsystem.Kafka.controller;
+
+
+import com.example.alertsystem.Kafka.service.EmailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/email")
+public class EmailController {
+
+    @Autowired
+    private EmailService emailService;
+
+    @PostMapping("/send")
+    public String sendEmail(@RequestParam String to,
+                            @RequestParam String subject,
+                            @RequestParam String message) {
+        emailService.sendEmail(to, subject, message);
+        return "Email sent to: " + to;
+    }
+}
